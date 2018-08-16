3015f002056c4e413b1e09cd6b1423264f653bbd
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-16 16:09:10 -0400

    added first component

---
+++ minesweeper/src/Minesweeper.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Minesweeper extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Minesweeper;
