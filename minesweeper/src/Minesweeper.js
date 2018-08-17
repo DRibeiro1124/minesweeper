f6ebe8c6d308e17cbb57c2c31f7bb6c0a7b35d34
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-16 20:04:39 -0400

    added game board

--- minesweeper/src/Minesweeper.js
+++ minesweeper/src/Minesweeper.js
@@ -12,10 +12,39 @@ class Minesweeper extends Component {
         }
     }
 
+    componentDidMount() {
+        fetch(BASE_URL, {
+            method: "POST",
+            body: JSON.stringify({ difficulty: 0})
+        }).then(resp => resp.json())
+        .then(newGame => {
+            console.log("game", newGame);
+            this.setState({
+                game: newGame
+            })
+        })
+    }
+
     render() {
         return (
             <div>
-                
+               currently playing    {this.state.game.id}
+
+               {this.state.game.board.map((row, i) => {
+                   console.log("row", row, i)
+                   return (
+                       <div>
+                           {row.map((col, j) => {
+                               return <span className="squares">
+                                {this.state.game.board [i][j]}
+                                is at
+                               {`${i}, ${j}`} 
+                               </span>
+                           })}
+                       </div>
+                   )
+                   return
+               })} 
             </div>
         );
     }
